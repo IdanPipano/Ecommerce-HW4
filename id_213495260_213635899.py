8e52b8bf4feead109064708c8eee384666c47601
IdanPipano <102733803+IdanPipano@users.noreply.github.com>
2023-12-20 13:31:45 +0200

    ?

--- id_213495260_213635899.py
+++ id_213495260_213635899.py
@@ -1,25 +1,32 @@
+import numpy as np
+
+
 class BiddingAgent1:
     def __init__(self):
         pass
 
     def get_bid(self, num_of_agents, P, q, v):
-        return v / 1.
+        return v / np.exp(1)
 
     def notify_outcome(self, reward, outcome, position):
         pass
 
     def get_id(self):
-        return "id_123456789_98765432"
+        return "id_213495260_213635899"
 
 class BiddingAgent2:
     def __init__(self):
         pass
 
     def get_bid(self, num_of_agents, P, q, v):
-        return v / 1.
+        """
+        MUST BE O(num_of_agents) time and space complexity.
+        """
+        return v / np.exp(1)
 
     def notify_outcome(self, reward, outcome, position):
         pass
 
     def get_id(self):
-        return "id_123456789_98765432"
+        return "id_213495260_213635899"
+
